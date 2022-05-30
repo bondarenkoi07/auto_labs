746e8d7f5eabff93b4440b311a480eaa71ace196
Илья Бондаренко <IABondarenko@mai.ru>
2022-05-30 12:19:23 +0300

    lmao bottom text

--- main/views/update/update.py
+++ main/views/update/update.py
@@ -3,30 +3,53 @@ from django.utils.decorators import method_decorator
 from django.utils.text import slugify
 from django.views.generic import UpdateView
 
-
 from main.github_api.api import *
 from main import models
 
 
+class User(UpdateView):
+    model = models.GitHubUser
+    form_class = UserForm
+    template_name = "form/user.html"
+
+    def get_success_url(self):
+        return reverse("profile", kwargs={"pk": self.object.pk})
+
+
 class Group(UpdateView):
     model = models.Group
+    form_class = GroupForm
+    template_name = "form/group.html"
+
+    def get_success_url(self):
+        return reverse("read-group", kwargs={"pk": self.object.pk})
 
 
-class TaskUpdateDescription(UpdateView):
+class Task(UpdateView):
     model = models.Task
+    form_class = TaskForm
+    template_name = "form/task.html"
+
+    def get_success_url(self):
+        return reverse("read-task", kwargs={"pk": self.object.pk})
 
 
-class TaskSend(UpdateView):
-    model = models.Task
-    form_class = UploadFileForm
-
-
-class ActionFile(UpdateView):
+class Action(UpdateView):
     model = models.Action
+    form_class = SubjectForm
+    template_name = "form/action.html"
+
+    def get_success_url(self):
+        return reverse("read-action", kwargs={"pk": self.object.pk})
 
 
 class Subject(UpdateView):
     model = models.Subject
+    form_class = SubjectForm
+    template_name = "form/subject.html"
+
+    def get_success_url(self):
+        return reverse("read-subject", kwargs={"pk": self.object.pk})
 
 
 @method_decorator(login_required)
